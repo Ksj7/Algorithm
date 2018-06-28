e74fee7d1adbd234204c6a9d24a6960b05408d39
sujin Kim <sujin.kim1414@gmail.com>
2018-06-29 03:08:54 +0900

    Update 3047.java

--- problem/3047.java
+++ problem/3047.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,40 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[3];
-        int tt[] = new int[3];
-        char str[] = new char[3];
-        int max = 0;
-        int min = 0;
-        int mid = 0;
         for (int i = 0; i < 3; i++) {
             arr[i] = sc.nextInt();
-            if (arr[max] < arr[i]) {
-                max = i;
-            }
-            if (arr[min] > arr[i]) {
-                min = i;
-            }
         }
-
-        mid = (max == 0) ? ((min == 2) ? 1 : 2) : ((max == 1) ? ((min == 2) ? 0 : 2) : (min == 1) ? 0 : 1);
-
         sc.nextLine();
-        String tmp = sc.nextLine();
-        for (int i = 0; i < 3; i++)
-            str[i] = tmp.charAt(i);
-
-        for (int i = 0; i < 3; i++) {
-            if (str[i] == 'A') {
-                tt[i] = arr[min];
-            } else if (str[i] == 'B'){
-                tt[i] = arr[mid];
-            }else{
-                tt[i] = arr[max];
-            }
-        }
-
-
-        System.out.print(tt[0] + " " + tt[1] + " " + tt[2]);
+        String a = sc.nextLine();
+        Arrays.sort(arr);
+        System.out.print(arr[a.charAt(0)-'A'] + " " + arr[a.charAt(1)-'A'] + " " + arr[a.charAt(2)-'A']);
 
 
     }
