bb3dd208ab179ccf4596125fcfdc27350361c995
sujin Kim <sujin.kim1414@gmail.com>
2018-09-21 00:30:35 +0900

    https://www.acmicpc.net/problem/10951

---
+++ problem/10951.cpp
@@ -0,0 +1,18 @@
+//
+//  10951.cpp
+//  codingtest
+//
+//  Created by sujin.kim on 2018. 9. 21..
+//  Copyright © 2018년 sujin.kim. All rights reserved.
+//
+
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    while(scanf("%d %d",&a, &b)!=EOF){
+        printf("%d\n",a+b);
+    }
+}
